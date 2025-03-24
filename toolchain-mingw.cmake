a4499965cad174d549dec90d797c03cab07733a6
Maks Rawski <maksymilian.rawski@tutanota.com>
2025-03-24 19:51:22 +0100

    cross compilation works! though things are ugly...

--- toolchain-mingw.cmake
+++ toolchain-mingw.cmake
@@ -2,12 +2,16 @@ set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
 # Set MinGW-w64 compiler paths
+set(CMAKE_AUTOMOC OFF)
 set(CMAKE_C_COMPILER x86_64-w64-mingw32-gcc)
 set(CMAKE_CXX_COMPILER x86_64-w64-mingw32-g++)
 set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
 
-# Specify Qt for Windows installation path
+# Specify libraries' installation paths
 set(Qt6_DIR /opt/Qt/6.8.2/mingw_64/lib/cmake/Qt6)
+# set(OpenCV_DIR /usr/x86_64-w64-mingw32/lib/cmake/opencv4)
+# set(OpenCV_DIR "/usr/x86_64-w64-mingw32/lib/cmake/opencv4" CACHE PATH "Path to OpenCV")
+
 
 # Set the root paths for MinGW and Qt
 set(CMAKE_FIND_ROOT_PATH
